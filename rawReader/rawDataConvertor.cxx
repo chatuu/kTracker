2d89425c8e69d9e735470de7ae1dc454e0a60c26
Kun Liu <liuk.pku@gmail.com>
2014-08-20 14:17:51 -0400

    Included the spill info and target position

--- rawReader/rawDataConvertor.cxx
+++ rawReader/rawDataConvertor.cxx
@@ -50,8 +50,44 @@ int main(int argc, char **argv)
 
   saveTree->Branch("rawEvent", &rawEvent, 256000, 99);
 
+  //Extract spill information
+  int runID = atoi(argv[3]);
+  int nSpills = 0;
+  int spillID[500];
+  int targetPos[500];
+  int first[500];
+  int last[500];
+
+  int m_runID;
+  int m_spillID;
+  int m_targetPos;
+  int m_first;
+  int m_last;
+
+  TFile* spillFile = new TFile("spills.root", "READ");
+  TTree* spillTree = (TTree*)spillFile->Get("save");
+
+  spillTree->SetBranchAddress("runID", &m_runID);
+  spillTree->SetBranchAddress("spillID", &m_spillID);
+  spillTree->SetBranchAddress("targetPos", &m_targetPos);
+  spillTree->SetBranchAddress("firstEvent", &m_first);
+  spillTree->SetBranchAddress("lastEvent", &m_last);
+
+  for(int i = 0; i < spillTree->GetEntries(); ++i)
+    {
+      spillTree->GetEntry(i);
+      if(m_runID != runID) continue;
+
+      spillID[nSpills] = m_spillID;
+      targetPos[nSpills] = m_targetPos;
+      first[nSpills] = m_first;
+      last[nSpills] = m_last;
+
+      ++nSpills;
+    }
+
   int nEvents = dataTree->GetEntries();
-  cout << "Totally " << nEvents << " events in this run" << endl;
+  cout << "Totally " << nEvents << " events in this run, " << nSpills << " spills are loaded ..." << endl;
   
   if(argc > 5) nEvents = atoi(argv[5]);
   for(int i = 0; i < nEvents; ++i)
@@ -59,9 +95,20 @@ int main(int argc, char **argv)
       //read the CODA data
       dataTree->GetEntry(i);
 
+      int idx = -1;
+      for(int j = 0; j < nSpills; ++j)
+	{
+	  if(codaEvent->getPhysEventID() <= last[j] && codaEvent->getPhysEventID() >= first[j])
+	    {
+	      idx = j;
+	      break;
+	    }
+	}
+      if(idx < 0 || idx >= nSpills) continue;
+
       //Get event level info
-      rawEvent->setEventInfo(codaEvent->getRunID(), codaEvent->getSpillID(), codaEvent->getPhysEventID());
-      //rawEvent->setTargetPos(codaEvent->getTargetPos()); //may need update later
+      rawEvent->setEventInfo(codaEvent->getRunID(), spillID[idx], codaEvent->getPhysEventID());
+      rawEvent->setTargetPos(targetPos[idx]); //may need update later
       rawEvent->setTriggerBits(rawEvent->getTriggerBits());  //may need understand different between TS and my own definition
       rawEvent->setTurnID(codaEvent->getTurnID());
       rawEvent->setRFID(codaEvent->getRFID());
@@ -81,8 +128,15 @@ int main(int argc, char **argv)
 	  h.tdcTime = h_coda.tdcTime;
 	  h.pos = p_geomSvc->getMeasurement(h.detectorID, h.elementID);
 	  h.driftDistance = p_geomSvc->getDriftDistance(h.detectorID, h.tdcTime);
-	  if(p_geomSvc->isInTime(h.detectorID, h.tdcTime)) h.setInTime();
-	
+	  if(h.detectorID > 24 && h.detectorID <= 40)
+	    {
+	      if(h_coda.inTime > 0) h.setInTime(); 
+	    }
+	  else if(p_geomSvc->isInTime(h.detectorID, h.tdcTime)) 
+	    {
+	      h.setInTime();
+	    }
+
 	  rawEvent->insertHit(h);
 	}
 
