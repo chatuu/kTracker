0046b26d2c939f4a05a8b0bcec9606a1feec9ef5
Kun Liu <liuk.pku@gmail.com>
2015-01-11 21:53:48 -0700

    Added a few lines of example code of sample analyzier

--- analysis_tools/sample.cxx
+++ analysis_tools/sample.cxx
@@ -13,9 +13,12 @@
 #include <TH1D.h>
 #include <TH1F.h>
 #include <TCanvas.h>
+#include <TClonesArray.h>
 
 #include "GeomSvc.h"
 #include "SRawEvent.h"
+#include "SRecEvent.h"
+#include "FastTracklet.h"
 
 using namespace std;
 
@@ -24,26 +27,37 @@ int main(int argc, char *argv[])
   GeomSvc* p_geomSvc = GeomSvc::instance();
   p_geomSvc->init(GEOMETRY_VERSION);
 
+  //Input structure
   SRawEvent* rawEvent = new SRawEvent();
+  SRecEvent* recEvent = new SRecEvent();
+  TClonesArray* tracklets = new TClonesArray("Tracklet");
 
   TFile* dataFile = new TFile(argv[1], "READ");
   TTree* dataTree = (TTree *)dataFile->Get("save");
 
   dataTree->SetBranchAddress("rawEvent", &rawEvent);
+  dataTree->SetBranchAddress("recEvent", &recEvent);
+  dataTree->SetBranchAddress("tracklets", &tracklets);
 
-  SRawEvent* rawEvent_new = new SRawEvent();
-
+  //Output structure
   TFile* saveFile = new TFile(argv[2], "recreate");
   TTree* saveTree = new TTree("save", "save");
-
-  saveTree->Branch("rawEvent", &rawEvent_new, 256000, 99);
 
   for(Int_t i = 0; i < dataTree->GetEntries(); ++i)
     {
       dataTree->GetEntry(i);
 
+      for(Int_t j = 0; j < recEvent->getNTracks(); ++j)
+	{
+	  SRecTrack track = recEvent->getTrack(j);
+	}
+
+      for(Int_t j = 0; j < tracklets->GetEntries(); ++j)
+	{
+	  Tracklet* track = (Tracklet*)tracklets->At(j);
+	}
+
       rawEvent->clear();
-      rawEvent_new->clear();
     }
   
   saveFile->cd();
