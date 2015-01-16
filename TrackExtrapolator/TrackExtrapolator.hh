a03cbcad14f0d40293a0e1e811278a239cb1d12c
Kun Liu <liuk.pku@gmail.com>
2015-01-16 12:16:36 -0600

    Added interface in TrackExtrapolator to allow settings of fMag and kMag strength

--- TrackExtrapolator/TrackExtrapolator.hh
+++ TrackExtrapolator/TrackExtrapolator.hh
@@ -43,7 +43,7 @@ public:
   ~TrackExtrapolator();
 
   ///Initialize geometry and physics
-  bool init(std::string geometrySchema);
+  bool init(std::string geometrySchema, double fMagStr = -999., double kMagStr = -999.);
 
   ///Set input initial state parameters
   void setInitialStateWithCov(double z_in, TMatrixD& state_in, TMatrixD& cov_in);
